ddc5c345e37ba9b1dc76e6ef35ba90065426ab80
mike <mikekarpenko@gmail.com>
2021-04-09 16:58:12 +0300

    add 2 pages

--- build/js/slick.config.js
+++ build/js/slick.config.js
@@ -1,4 +1,35 @@
 
+$('.mainslider').slick({
+	slidesToScroll: 1,
+	autoplay: false,
+	dots: false,
+	infinite: true,
+	autoplaySpeed: 3000,
+	slidesToShow: 1,
+	// centerMode: true,
+	responsive: [
+		{
+			breakpoint: 990,
+			settings: {
+				arrows: true,
+				// centerMode: true,
+				centerPadding: '0px',
+				slidesToShow: 1
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				// centerMode: true,
+				centerPadding: '10px',
+				slidesToShow: 1
+			}
+		}
+	]
+});
+
+
 $('.docslider').slick({
 	slidesToScroll: 1,
 	autoplay: false,
